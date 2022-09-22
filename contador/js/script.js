96e5e29baea44e9c1f7e6f76cc64559233c3808b
Gabriel Cruz <itsgabrielcruz@gmail.com>
2022-09-21 22:00:10 -0300

    fix-bug

--- contador/js/script.js
+++ contador/js/script.js
@@ -2,7 +2,7 @@ function contar() {
     var chamadosAtuados = document.querySelector("#qtd");
     var chamadosAtuados = chamadosAtuados.value;
 
-    if(chamadosAtuados.length > 20 && chamadosAtuados.length < 20) {
+    if(chamadosAtuados.length > 20 && chamadosAtuados.length < 30) {
         alert(`Você atuou em ${chamadosAtuados.length} chamados. Parabéns, você ultrapassou a meta diária.`);
     } else if(chamadosAtuados.length === 20) {
         alert(`Você atuou em ${chamadosAtuados.length} chamados. Você atingiu a meta diária. Parabéns!`);
