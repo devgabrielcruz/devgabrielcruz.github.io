6c599f2b2cd4406d85279a4fcce79b6709e39f55
Gabriel Cruz <itsgabrielcruz@gmail.com>
2022-09-21 23:30:22 -0300

    update 2.0

--- contador/js/script.js
+++ contador/js/script.js
@@ -4,16 +4,16 @@ function contar() {
     var chamadosAtuados = chamadosAtuados.value;
 
     if(chamadosAtuados.length > 20 && chamadosAtuados.length < 30) {
-        content.innerHTML = `Você atuou em <span>${chamadosAtuados.length} chamados</span>. Parabéns, você ultrapassou a meta diária.`;
-
+        content.innerHTML = `Você atuou em <span class='green'>${chamadosAtuados.length} chamados</span>. Parabéns, você ultrapassou a meta diária.`;
+        document.querySelector("#botao").innerHTML = "<a href=''>Assita o EP via Player Externo</a>";
     } else if(chamadosAtuados.length === 20) {
-        content.innerHTML = `Você atuou em <span>${chamadosAtuados.length} chamados</span>. Você atingiu a meta diária. Parabéns!`;
+        content.innerHTML = `Você atuou em <span class='green'>${chamadosAtuados.length} chamados</span>. Você atingiu a meta diária. Parabéns!`;
 
     } else if(chamadosAtuados.length >= 30) {
-        content.innerHTML = `UAU! Você é uma máquina! Conseguiu atuar em <span>${chamadosAtuados.length} chamados</span> hoje.`;
+        content.innerHTML = `UAU! Você é uma máquina! Conseguiu atuar em <span class='green'>${chamadosAtuados.length} chamados</span> hoje.`;
 
     } else {
-        content.innerHTML = `Você atuou em <span>${chamadosAtuados.length} chamados</span>. Faltou <span>${20 - chamadosAtuados.length}</span> para alcançar a meta diária.`;
+        content.innerHTML = `Você atuou em <span class='red'>${chamadosAtuados.length} chamados</span>. Faltou <span>${20 - chamadosAtuados.length}</span> para alcançar a meta diária.`;
 
     }
 
