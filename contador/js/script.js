af506d7a2fefb2a2e1a8581bf6df2c59c5ad325f
Gabriel Cruz <itsgabrielcruz@gmail.com>
2022-09-21 23:08:30 -0300

    update 2.0

--- contador/js/script.js
+++ contador/js/script.js
@@ -1,15 +1,23 @@
 function contar() {
     var chamadosAtuados = document.querySelector("#qtd");
+    var content = document.querySelector('#content');
     var chamadosAtuados = chamadosAtuados.value;
 
     if(chamadosAtuados.length > 20 && chamadosAtuados.length < 30) {
-        alert(`Você atuou em ${chamadosAtuados.length} chamados. Parabéns, você ultrapassou a meta diária.`);
+        content.innerHTML = `Você atuou em <span>${chamadosAtuados.length} chamados</span>. Parabéns, você ultrapassou a meta diária.`;
+
     } else if(chamadosAtuados.length === 20) {
-        alert(`Você atuou em ${chamadosAtuados.length} chamados. Você atingiu a meta diária. Parabéns!`);
+        content.innerHTML = `Você atuou em <span>${chamadosAtuados.length} chamados</span>. Você atingiu a meta diária. Parabéns!`;
+
     } else if(chamadosAtuados.length >= 30) {
-        alert(`UAU! Você é uma máquina! Conseguiu atuar em ${chamadosAtuados.length} chamados hoje.`);
+        content.innerHTML = `UAU! Você é uma máquina! Conseguiu atuar em <span>${chamadosAtuados.length} chamados</span> hoje.`;
+
     } else {
-        alert(`Você atuou em ${chamadosAtuados.length} chamados. Faltou ${20 - chamadosAtuados.length} para alcançar a meta diária.`);
+        content.innerHTML = `Você atuou em <span>${chamadosAtuados.length} chamados</span>. Faltou <span>${20 - chamadosAtuados.length}</span> para alcançar a meta diária.`;
+
     }
 
+    var resultado = document.querySelector('#atuados');
+    resultado.innerHTML= `${chamadosAtuados.length} chamados atuados.`;
 }
+
